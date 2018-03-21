334ba6370f1cefe85adf8a39e93bf953e5d14fe6
Manuel Mörtl <manuel.moertl@student.tugraz.at>
2018-03-21 10:34:12 +0100

    started base project

---
+++ app/src/androidTest/java/at/maymay/convertme/ConverterInstrumentedTest.java
@@ -0,0 +1,31 @@
+package at.maymay.convertme;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ConverterInstrumentedTest {
+    @Rule
+    public ActivityTestRule<Converter> myActivityRule = new ActivityTestRule<>(Converter.class);
+
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("at.maymay.convertme", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/at/maymay/convertme/Converter.java
@@ -0,0 +1,13 @@
+package at.maymay.convertme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Converter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_converter);
+    }
+}
