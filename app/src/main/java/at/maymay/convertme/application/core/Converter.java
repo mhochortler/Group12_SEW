021474133568eba52e93419c125b823b1db14198
Michael Hochörtler <m.hochoertler@student.tugraz.at>
2018-04-17 17:31:31 +0200

    [AB,HM] implemented raw version of the database

--- app/src/main/java/at/maymay/convertme/application/core/Converter.java
+++ app/src/main/java/at/maymay/convertme/application/core/Converter.java
@@ -7,22 +7,9 @@ import at.maymay.convertme.R;
 
 public class Converter extends AppCompatActivity {
 
-    private double fromValue;
-    private double toValue;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_converter);
-
-        UnitHelper helper = new UnitHelper();
-        helper.init();
-
     }
-
-    public void convert(Unit from, Unit to) {
-
-    }
-
-
 }
