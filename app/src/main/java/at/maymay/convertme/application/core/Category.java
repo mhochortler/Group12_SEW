14966738b0172bb7cb859f09fd7731211783a0ea
Michael Hochörtler <m.hochoertler@student.tugraz.at>
2018-04-18 22:19:22 +0200

    [AB,HM] implemented new-conversion-line functionality

--- app/src/main/java/at/maymay/convertme/application/core/Category.java
+++ app/src/main/java/at/maymay/convertme/application/core/Category.java
@@ -12,4 +12,14 @@ public abstract class Category {
     public List<Unit> getUnitList(){
         return unit_list_;
     }
+
+    public String[] getStringifytUnitList() {
+        List<String> result = new ArrayList<>();
+
+        for(Unit u : unit_list_) {
+            result.add(u.getShortcut());
+        }
+
+        return result.toArray(new String[result.size()]);
+    }
 }
