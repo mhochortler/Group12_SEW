13a964f07eef2bf11afc3111eda636022412e953
julh16 <jul.holzegger@hotmail.com>
2018-05-02 08:59:55 +0200

    [HJ,AB] Corrected weight-units factors

--- app/src/main/java/at/maymay/convertme/application/core/Weight.java
+++ app/src/main/java/at/maymay/convertme/application/core/Weight.java
@@ -10,11 +10,11 @@ public class Weight extends Category {
     protected void init() {
         Unit kg = new Unit("Kilogram", "kg", 1000.0);
         Unit dag = new Unit("Decagram", "dag", 10.0);
-        Unit oz = new Unit("Ounce", "oz", 0.0283495);
-        Unit lb = new Unit("Pound", "lb", 0.453592);
-        Unit st = new Unit("Stone", "st", 6.35029);
-        Unit ust = new Unit("US Ton", "ust", 907.18428568);
-        Unit it = new Unit ("Imperial Ton", "it", 1016.05);
+        Unit oz = new Unit("Ounce", "oz", 28.349);
+        Unit lb = new Unit("Pound", "lb", 453.592);
+        Unit st = new Unit("Stone", "st", 6350.29);
+        Unit ust = new Unit("US Ton", "ust", 907184.28568);
+        Unit it = new Unit ("Imperial Ton", "it", 1016050);
         unit_list_.add(kg);
         unit_list_.add(dag);
         unit_list_.add(oz);
