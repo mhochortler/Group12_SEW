0abbe3455b0603ba743648236e258555c6ae9711
Felix Pallua <felix.pallua@student.tugraz.at>
2018-05-02 08:00:52 +0200

    [PF,CK] Merge from branch develop, get exchange rates at initialization

--- app/src/main/java/at/maymay/convertme/application/core/Currency.java
+++ app/src/main/java/at/maymay/convertme/application/core/Currency.java
@@ -8,15 +8,28 @@ public class Currency extends Category {
 
     @Override
     protected void init() {
-        Unit usd = new Unit("U.S. Dollar", "USD");
-        Unit eur = new Unit("Euro", "EUR");
-        Unit yen = new Unit("Yen", "JPY");
-        Unit rubel = new Unit("British Pound", "GBP");
-        Unit frank = new Unit("Swiss Franc", "CHF");
+
+        Unit usd = new Unit("U.S. Dollar", "USD", 1.0);
+        Unit eur = new Unit("Euro", "EUR", getExchangeRate("EUR"));
+        Unit yen = new Unit("Yen", "JPY", getExchangeRate("JPY"));
+        Unit pound = new Unit("British Pound", "GBP", getExchangeRate("GBP"));
+        Unit frank = new Unit("Swiss Franc", "CHF", getExchangeRate("CHF"));
         unit_list_.add(usd);
         unit_list_.add(eur);
         unit_list_.add(yen);
-        unit_list_.add(rubel);
+        unit_list_.add(pound);
         unit_list_.add(frank);
+    }
+
+    private double getExchangeRate(String currency)
+    {
+        double result = 0.0;
+        try{
+            result = CurrencyExchangeAPI.getExchangeRate(currency, "USD");
+        }
+        catch(Exception e){
+            System.out.print("Couldn't get exchange rate");
+        }
+        return result;
     }
 }
